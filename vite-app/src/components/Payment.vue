e07b8117b31aa105e1ca82aca3132de90f121cdc
hita-fttk <fttkmcc77@gmail.com>
2023-10-30 20:19:01 +0900

    Payment-componentを追加した。

---
+++ vite-app/src/components/Payment.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="container">
+        <h1>最近の支出</h1>
+        <div class="payment">
+            <label>カレーライス</label>
+            <label>400円</label>
+    </div>
+    </div>
+
+</template>
+
+<style scoped>
+    h1{
+        font-size: 60px;
+    }
+    label{
+        font-size: 20px;
+        font-weight: bold;
+        height: 60px;
+    }
+    .container{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    .payment{
+        display: flex;
+        justify-content: space-between;
+        height:80px;
+        width: 400px;
+        background-color: aliceblue;
+        align-items: center;
+        padding-top: 12px;
+
+    }
+</style>
