679fb44a057d37569d757f93829cc18af6f95312
hita-fttk <fttkmcc77@gmail.com>
2023-10-31 10:11:44 +0900

    create Tweet.vue

---
+++ vite-app/src/components/Tweet.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+const tweets = [{id:'0',description:'Hello world!'},{id:'1',description:'this is second tweet'}]
+</script>
+
+<template>
+  <div class="container">
+    <h1>Tweeter</h1>
+    <div class="form-container">
+      <input type="text">
+      <button>post</button>
+    </div>
+    <div class="tweet-container">
+      <ul>
+        <li v-for="tweet in tweets" :key="tweet.id" class="tweet-list">
+          <span>{{ tweet.description }}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.form-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: aliceblue;
+  padding: 24px 0;
+  width: 60%;
+  margin-bottom: 12px;
+  border-radius: 4px;
+}
+h1{
+  font-size: 80px;
+}
+.form-area{
+  height: 40px;
+  background-color:yellowgreen;
+  padding: 40px;
+  display: flex;
+  align-items: center;
+}
+input{
+  border: 1px solid;
+  margin-top: 16px;
+}
+button{
+  background-color: aquamarine;
+  border: 1px solid;
+  border-radius: 2px;
+  margin-top: 16px;
+  padding: 4px 16px;
+  color: azure;
+  font-weight: bold;
+}
+.tweet-list{
+  list-style: none;
+}
+</style>
